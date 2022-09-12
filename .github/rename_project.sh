c9da47ec99e227857b5dd58f1ba47805b237014c
pattyvuk <pattyvuk@users.noreply.github.com>
2022-09-12 01:14:53 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="pattyvuk"
+original_name="pythonx"
+original_urlname="pythonX"
+original_description="Awesome pythonx created by pattyvuk"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv pythonx $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
